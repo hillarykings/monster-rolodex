c5bd96020862ed16d8171b75e677379d15ee8e8d
Hillarykings <hillarypeters23@gmail.com>
2020-01-18 21:39:19 +0100

    Updates

---
+++ precache-manifest.0355d52e936249e04d88fdb9db9066eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bf534b11cc31bb87fec18e1ed6a0e9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3462c8550dd2b11d321f",
+    "url": "/monster-rolodex/static/css/main.dc2f007c.chunk.css"
+  },
+  {
+    "revision": "e9bf521eeebd951c5100",
+    "url": "/monster-rolodex/static/js/2.7a15211e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7a15211e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3462c8550dd2b11d321f",
+    "url": "/monster-rolodex/static/js/main.72656bd0.chunk.js"
+  },
+  {
+    "revision": "b36270675cbae4de8ea0",
+    "url": "/monster-rolodex/static/js/runtime-main.b0579f01.js"
+  }
+]);
